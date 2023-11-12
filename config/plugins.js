3fba04100f55940c9ff03d5f1fbff370fa50fec3
Chris Siku <chrissiku5@gmail.com>
2023-11-12 15:10:28 +0200

    Initial and save project

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: 'dknvsbuyy',
-        api_key: '117364963376755',
-        api_secret: 'xmCRa0mRgRO9CxjtVkRJ0xRhGek',
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
